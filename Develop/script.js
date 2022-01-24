eb3bf9464395148099a9d5404b4cf88110c3cbb4
unknown <pearsenation@gmail.com>
2022-01-24 13:01:03 -0500

    Got the generatePassword function running with window prompts and alerts.

--- Develop/script.js
+++ Develop/script.js
@@ -10,5 +10,39 @@ function writePassword() {
 
 }
 
+function generatePassword() {
+  var seedString = '';
+  
+  var passwordLength = prompt('Choose password length between 8 and 128 characters (ex. "15").'); console.log(passwordLength); //Prompt for getting a length value and a following while statement to return the prompt if the value is out of bounds continuously.
+  while (passwordLength < 8 || passwordLength > 128){
+    passwordLength = prompt('The password must be between 8 and 128 characters (ex. "15"). Choose password length.'); console.log(passwordLength);
+  }
+  
+  var lowerCase = prompt('Would you like to include lowercase letters? Y/N'); // Prompt for including lowercase charcters into the seedString.
+  if (lowerCase.toLowerCase() == 'y'){
+    seedString = 'qwertyuiopasdfghjklzxcvbnm';
+  } console.log(lowerCase);
+  
+  var upperCase = prompt('Would you like to include uppercase letters? Y/N'); console.log(upperCase); // Prompt just like for lowercase, but the seedString is concatinated if the boolean is true.
+  if (lowerCase.toLowerCase() == 'y'){
+    seedString = seedString + seedString.toUpperCase();
+  }
+
+  var specialCase = prompt('Would you like to include special characters? Y/N'); console.log(upperCase); // Prompt for special characters set up just like before.
+  if (lowerCase.toLowerCase() == 'y'){
+    seedString = seedString + 'q!@#$%^&*()_+';
+  }
+
+  if (seedString === ''){ // Alert the user if the seedString is empty and ask them to start over.
+    alert('At least one case selection must be made (lower, upper, and/or special characters). Please start again.');
+  }
+
+  for (i = 0; i < passwordLength; i++){ // Finally, use a random character finder to create a new string (password) using the seedString with a number of characters equal to the passwordLength.
+    password += seedString.charAt(Math.floor(Math.random() * passwordLength));
+  }
+
+  return password;
+}
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
